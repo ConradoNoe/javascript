15f51f2fca652b745e80efa639d67017a0f206d5
conrado noe <conradonoee@gmail.com>
2023-11-01 20:36:25 -0300

    commit final primer entrega

--- javascript/main.js
+++ javascript/main.js
@@ -1,33 +1,57 @@
-function bienvenido(){
-    let ingresarNombre = prompt("ingresa tu nombre")
-    let ingresarApellido = prompt("ingresa tu apellido")
-    alert("bienvenido " + ingresarNombre + " " + ingresarApellido)
+function bienvenido() {
+    ingresarNombre = prompt("Ingresa tu nombre");
+    ingresarApellido = prompt("Ingresa tu apellido");
+    alert("Bienvenido " + ingresarNombre + " " + ingresarApellido);
+    console.log("el nombre ingresado es " + ingresarNombre)
 }
 
-function cambioDolares(dolsHoy,tusPesos){
-    alert("buenos dias, hoy el dolar esta 950 pesos")
-    dolsHoy = 950
-    tusPesos = parseFloat(prompt("ingresa cuantos pesos queres cambiar"))
-    let resultadoCambio = tusPesos / dolsHoy
-    alert("tenes " + resultadoCambio + " dolares")
-}
-function cambioReales(realesHoy,tusPesos1){
-    alert("buenos dias, hoy el real esta 69 pesos")
-    realesHoy = 69
-    tusPesos1 = parseFloat(prompt("ingresa cuantos pesos queres cambiar"))
-    let resultadoCambio1 = tusPesos1 / realesHoy
-    alert("tenes " + resultadoCambio1 + " reales")
-}
-function cambioEuros(eurosHoy,tusPesos2){
-    alert("buenos dias, hoy el euro esta 1100 pesos")
-    eurosHoy = 1100
-    tusPesos2 = parseFloat (prompt("ingresa cuantos pesos queres cambiar"))
-    let resultadoCambio2 = tusPesos2 / eurosHoy
-    alert("tenes " + resultadoCambio2 + " euros")
+function edad() {
+    let edadIngresada = parseInt(prompt("Ingresa tu edad"));
+    if (edadIngresada < 18) {
+        alert("Eres menor de edad y no puedes realizar esta operación");
+        console.log("el usuario es menor de edad se cancela la operacion")
+        return false;
+    } else {
+        return true;
+    }
 }
 
+function cambio() {
+    let permitido = edad(); 
 
-bienvenido()
-cambioDolares()
-cambioReales()
-cambioEuros()
+    if (permitido) {
+        let tipoDeCambio = prompt("Escribe a continuación qué moneda quieres cambiar: dólares, euros o reales, o si quieres cancelar, escribe 'esc'");
+    
+        while (tipoDeCambio !== 'esc') {
+            switch (tipoDeCambio) {
+                case "dolares":
+                    let cambioDolares = 950;
+                    let montoDolares = parseFloat(prompt("Ingresa el monto que quieras cambiar a dólares"));
+                    let totalDolares = montoDolares / cambioDolares;
+                    alert("Usted recibirá " + totalDolares + " Dolares");
+                    break;
+                case "euros":
+                    let cambioEuros = 1100;
+                    let montoEuros = parseFloat(prompt("Ingresa el monto que quieras cambiar a euros"));
+                    let totalEuros = montoEuros / cambioEuros;
+                    alert("Usted recibirá " + totalEuros + " Euros");
+                    break;
+                case "reales":
+                    let cambioReales = 70;
+                    let montoReales = parseFloat(prompt("Ingresa el monto que quieras cambiar a reales"));
+                    let totalReales = montoReales / cambioReales;
+                    alert("Usted recibirá " + totalReales + " Reales");
+                    break;
+                default:
+                    alert("Los datos ingresados son incorrectos");
+                    break;
+            }
+            tipoDeCambio = prompt("Si quieres seguir cambiando, vuelve a ingresar el tipo de moneda: dolares, euros, reales. Si ya terminaste la operación, escribe 'esc' para salir");
+        }
+        alert("Gracias por utilizar la funcion de cambio.");
+    }
+}
+
+bienvenido();
+cambio();
+
